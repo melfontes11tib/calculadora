b3f51d9d78d068f354ab5b30d64d7ec21d807acf
mel fontesss <m26fontes@gmail.com>
2025-04-04 02:18:01 +0000

    termino do projeto

--- scrpt.js
+++ scrpt.js
@@ -1,25 +1,61 @@
-let conta = 0
-let pessoas = 0
- 
-  const contaInput = document.querySelector("#conta")
-  contaInput.addEventListener("input", receberValorConta)
-  
-   function receberValorConta(evento) {
-    conta = Number(evento.target.value)
+let valorConta = 0;
+let numPessoas = 0;
+let taxaGorjeta = 0;
+
+const inputConta = document.querySelector("#conta");
+inputConta.addEventListener("input", atualizarConta);
+
+function atualizarConta(e) {
+  valorConta = Number(e.target.value);
+  atualizarCalculo();
+}
+
+const inputPessoas = document.querySelector("#pessoas");
+inputPessoas.addEventListener("input", atualizarPessoas);
+
+function atualizarPessoas(e) {
+  const erroTexto = document.querySelector(".pessoas #erro");
+  const caixaInput = document.querySelector(".pessoas .input-box");
+
+  if (e.target.value === "0") {
+    erroTexto.style.display = "block";
+    caixaInput.setAttribute("id", "erro-div");
+  } else {
+    erroTexto.style.display = "none";
+    caixaInput.removeAttribute("id");
+    numPessoas = Number(e.target.value);
   }
-  
-  function receberQuantidadePessoa(evento) {
-    const paragrafoErro = document.querySelector(".pessoas #erro")
-    const divErro = document.querySelector(".pessoas .input-box")
-  
-    if (evento.target.value === "0") {
-      paragrafoErro.style.display = "block"
-      divErro.setAttribute("id", "erro-div")
-    } else {
-      paragrafoErro.style.display = "none"
-      divErro.setAttribute("id", "")
-      pessoas = Number(evento.target.value)
+
+  atualizarCalculo();
+}
+
+const botoes = document.querySelectorAll(".gorjeta input[type='button']");
+
+botoes.forEach(btn => {
+  btn.addEventListener("click", definirGorjeta);
+});
+
+function definirGorjeta(e) {
+  botoes.forEach(btn => {
+    btn.classList.remove("botao-ativo");
+    if (btn.value === e.target.value) {
+      btn.classList.add("botao-ativo");
     }
+  });
+
+  taxaGorjeta = e.target.value !== "" ? parseFloat(e.target.value) / 100 : 0;
+  atualizarCalculo();
+}
+
+const inputCustom = document.querySelector("#outra");
+inputCustom.addEventListener("input", definirGorjeta);
+
+function atualizarCalculo() {
+  if (valorConta > 0 && taxaGorjeta > 0 && numPessoas > 0) {
+    const campoGorjeta = document.querySelector(".gorjeta-total > strong");
+    campoGorjeta.innerHTML = `R$ ${(valorConta * taxaGorjeta / numPessoas).toFixed(2)}`;
+
+    const campoTotal = document.querySelector(".total > strong");
+    campoTotal.innerHTML = `R$ ${((valorConta + valorConta * taxaGorjeta) / numPessoas).toFixed(2)}`;
   }
-  
-  
+}
